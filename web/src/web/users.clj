c0a4545e80d8bd7a381238d96c72ac478d263083
Dennis Lipovsky <dlipovsky@yodle.com>
2014-07-15 21:18:54 -0400

    having some trouble fetching users from the database

--- web/src/web/users.clj
+++ web/src/web/users.clj
@@ -7,13 +7,12 @@
               "postgresql://localhost:5432/ladders?user=ladders&password=password"))
 
 (defn find-user [username]
-  (sql/query spec ["select username, password from users where username = ?" username]))
+  (first (sql/query spec ["select username, password from users where username = ?" username])))
 
 (defn all []
   (sql/query spec ["select * from users"]))
 
 (defn create [{:keys [username password admin] :as user-data}]
   (let [bcrypt-password (creds/hash-bcrypt password)]
-    (println "create " username " " password)
     (sql/insert! spec :users {:username username :password bcrypt-password})
     {:identity username :password bcrypt-password}))
